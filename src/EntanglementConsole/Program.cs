dd93ac60d6a351123a37c611bfb0d159ccb94629
Guido Nieuwhof <guido.nieuwhof@gmail.com>
2017-08-13 18:13:39 +0200

    Added console project.

---
+++ src/EntanglementConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EntanglementConsole
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
